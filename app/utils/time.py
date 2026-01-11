2057acfd03ce6038a975b0bc6115828c31b05aaf
Pei Wu <starwu02@gmail.com>
2026-01-11 19:09:06 +0800

    feat: simplify get_current_window_timestamp to return only the start of the window

--- app/utils/time.py
+++ app/utils/time.py
@@ -7,6 +7,5 @@ def get_current_window_timestamp():
     INTERVAL = 900  # 15 * 60 sec
 
     window_start = int((now // INTERVAL) * INTERVAL)
-    window_end = window_start + INTERVAL
 
-    return window_start, window_end
+    return window_start 
